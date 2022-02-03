b9ce9bd6a1b98fe8a27e436e01274b40b2660e59
mborreros <mlbsindia@gmail.com>
2022-02-03 15:53:33 -0500

    commit tree 60ae901f876ca6a662dba48155311c956fdeedcf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(comparisson)
+
+    ["hello world zombies pants dipper"].filter(word)
+
+    # puts comparisson
+    # puts word
+  end
+
+end
